e22532652142ef01a42cdeaf19c19bf4937eb1cd
BL7Awy <mahadelr19@gmail.com>
2020-11-22 13:31:52 +0200

    completed algo

--- Algorithm/ATC.cpp
+++ Algorithm/ATC.cpp
@@ -13,105 +13,185 @@
 *   no overlapping between groups who have the same TA.
 *   for every group only one lecture at a day
 *   one professor one lecture at a day
-*   lecture and tutorial will have consecutive timeslots
-*   prof of a course teaches both lecture and tutorial
+*	each professor teaches at most 2 lectures in total
 *   professor can only choose a preferred timeslot not a day
 *   lecture will always be at either 1st or 2nd timeslots
-*	lectures are attended by all groups
+*   lecture and tutorial will have consecutive timeslots
+*   prof of a course teaches both lecture and tutorial
+*	courses are attended by all groups
 *	number of courses for a group is <= 5
-*	works for only one class(year)
-*	fields of CELL class should be changed to pointers
+*
+*	No rooms are assigned // UNDONE
+*	works for only one class(year) // UNDONE
+*
+*	Optimizations
+*	------------------
+*	Merge all input in one loop
+*	fields of CELL class should be changed to pointers //WITHDRAWN
+*	
+*
 */
-const int N = 505;
 
-vector<PROFESSOR> profs;
-vector<COURSE> courses;
-vector<ROOM> rooms;
-vector<CLASS> classes;
-vector<GROUP> groups;
-vector<PREFERENCES> prefs;
-vector<TA> TAs;
-vector<int> class_groups[N];
-vector<int> course_TAs[N];
-int day[N];
-int timeslot[N];
-	
-
-int row, col;
-CELL grid[5][7][5][6]; //[days][timeslot][year][group];
-
-void fill_lectures_and_tutorials()
+bool check(int prof_id, int year, int cur, int pref)
 {
-	int cnt = 0;
-	memset(timeslot, 0, sizeof timeslot);
-    memset(day, 0, sizeof day);
-    int prefers[profs.size()][7];
-    memset(prefers, 0, sizeof prefers);
-    for(int i=0; i<prefs.size(); i++) prefers[prefs[i].prof->id][prefs[i].time] = 1;
-    for(int i=0; i<classes.size(); i++)
-    {
-    	if(classes[i].type == 1)
-    	{
-    		for(int j=0; j<6; j++)
-	    	{
-	    		int pref = 1;
-	    		if(prefers[classes[i].course->prof->id][0]) pref = 0;
-	    		CELL &ref = grid[cnt][pref][groups[class_groups[i][0]].year-1][j];
-	    		ref.type = 1;
-	    		ref.course_name=classes[i].course->name;
-	    		ref.instructor_name=classes[i].course->prof->name;
-	    		day[classes[i].course->id] = cnt;
-	    		timeslot[classes[i].course->id] = pref;
-//	    		if(!j)
-//	    		{
-//	    			cout << ref.course_name << " " << ref.type << " " << ref.instructor_name << endl;
-//	    			cout << cnt << " " << pref << endl;
-//	    		}
-			}
-	    	cnt++;
-    	}
-	}
-	for(int i=0; i<classes.size(); i++)
+	for(int g=0; g<G; g++)
 	{
-		if(classes[i].type == 2)
+		for(int y=0; y<Y; y++)
 		{
-			for(int j=0; j<6; j++)
-			{
-				int idx = classes[i].course->id;
-				CELL &ref = grid[day[idx]][timeslot[idx]+1][groups[class_groups[i][0]].year-1][j];
-				ref.type = 2;
-				ref.course_name=classes[i].course->name;
-				ref.instructor_name=classes[i].course->prof->name;
-//				if(!j) 
-//					cout << ref.course_name << " " << ref.type << " " << ref.instructor_name << endl;
-	    	
-			}
-		}	
+			if(grid[cur][pref][y][g].type == 1 && 
+			grid[cur][pref][y][g].instructor_id == prof_id) return 1;
+			if(grid[cur][pref+1][y][g].type == 1 && 
+			grid[cur][pref+1][y][g].instructor_id == prof_id) return 1;
+		}
+		if(grid[cur][pref][year][g].type != -1) return 1;
+		if(grid[cur][pref+1][year][g].type != -1) return 1;
+	}
+	return 0;
+}
+
+int get_day(int prof_id, int year, int cur, int pref)
+{
+	while(check(prof_id, year, cur, pref)) cur += 2, cur %= 5;
+	return cur;	
+}
+
+void AssignLectureAndTutorial(int id, int day, int slot, int year, int group)
+{
+	CELL &lec = grid[day][slot][year][group];
+	lec.course_name = courses[id].name;
+	lec.type = 1;
+	lec.instructor_id = courses[id].prof->id;
+	lec.instructor_name = courses[id].prof->name;
+	lec.room_id = rooms[0][cnt[day][slot][0]].id;
+	
+	CELL &tut = grid[day][slot+1][year][group];
+	tut.course_name = courses[id].name;
+	tut.type = 2;
+	tut.instructor_id = courses[id].prof->id; 
+	tut.instructor_name = courses[id].prof->name;
+	tut.room_id = rooms[1][cnt[day][slot+1][1]].id;
+	if(group == G-1) cnt[day][1+slot][1]++, cnt[day][slot][0]++;
+}
+
+void AssignLab(int id, int day, int slot, int year, int group)
+{
+	int sz = courses[id].TAs.size();
+	int C = (G + sz - 1)/sz;
+	CELL &lab = grid[day][slot+2+(group%C)][year][group];
+	lab.type = 3;
+	lab.course_name = courses[id].name;
+	lab.instructor_id = TAs[courses[id].TAs[group/C]].id;
+	lab.instructor_name = TAs[courses[id].TAs[group/C]].name;
+	lab.room_id = rooms[2][cnt[day][slot+2+(group%C)][2]++].id;
+}
+
+
+void AssignCourse(int id, int pref, int cur)
+{
+	int year = courses[id].year-1;
+	for(int j=0; j<G; j++)
+	{
+		AssignLectureAndTutorial(id, cur, pref, year, j);
+		AssignLab(id, cur, pref, year, j);
 	}
 }
 
-void fill_labs()
+
+
+
+void GenerateTimetable()
 {
-	
-	for(int i=0; i<classes.size(); i++)
+	int cnt = 0;
+	int cur[6] = {};
+	int year_day[5][5] = {{}};
+	int prof_day[profs.size()][5] = {{}};
+    int prefers[profs.size()][7];
+    memset(prefers, 0, sizeof prefers);
+    for(int i=0; i<prefs.size(); i++) prefers[prefs[i].prof->id][prefs[i].time] = 1;
+    for(int i=0; i<courses.size(); i++)
+    {
+		int pref = 1;
+		int prof_id = courses[i].prof->id;
+		if(prefers[prof_id][0]) pref = 0;
+		int year = courses[i].year-1;
+		cur[year] = get_day(prof_id, year, cur[year], pref);
+    	AssignCourse(courses[i].id, pref, cur[year]);
+		cur[year]+=2, cur[year] %= 5;
+	}
+}
+
+
+void FillSchedClasses()
+{
+	int cnt = 1;
+	for(int d=0; d<D; d++)
 	{
-		if(classes[i].type != 3) continue;
-		int idx = classes[i].course->id;
-		int G = class_groups[i].size();
-		int C = G/course_TAs[i].size();
-		int year = groups[class_groups[i][0]].year-1;
-		for(int j=0; j<G; j++)
+		for(int t=0; t<T; t++)
 		{
-			CELL &ref = grid[day[idx]][timeslot[idx]+2+(j%C)][year][j];
-			ref.type = 3;
-			ref.course_name=classes[i].course->name;
-			ref.instructor_name=TAs[course_TAs[i][j/C]].name;
+			for(int y=0; y<Y; y++)
+			{
+				for(int g=0; g<G; g++)
+				{
+					CELL &cell = grid[d][t][y][g];
+					if(cell.type == -1) continue;
+					SCHED_CLASS c;
+					c.room_id = cell.room_id+1;
+					c.id = cnt++;
+					c.timeslot = t+1;
+					c.day = d+1;
+					c.group_id = g+1;
+					c.instructor_id = cell.instructor_id+1;
+					c.type = cell.type;
+					output.push_back(c);
+				}
+			}
 		}
 	}
 }
 
+void PrintTimetable()
+{
+	string title(((15*G+1)*Y-1)/2, '*');
+	title = title + " TESTING PHASE " + title;
+	cout << title << endl << endl;
+	string tmp(13+(15*G+1)*Y, '-');
+	for(int d=0; d<D; d++)
+	{
+		cout << "Day: " << days[d] << endl;
+		for(int t=0; t<T; t++)
+		{
+			cout << slot[t] << " | ";
+			for(int i=0; i<Y; i++)
+			{
+				for(int j=0; j<G; j++)
+				{
+					CELL &ref = grid[d][t][i][j];
+					if(ref.type != -1)
+						cout << ref.course_name << " " << types[ref.type-1] 
+						<< " " << R[ref.room_id].number << " |"; 
+					else cout << "            " << " |";
+					if(j != 2) cout << " ";
+				}
+				cout << "| ";
+			}
+			cout << endl;
+		}
+		cout << tmp << endl;
+	}
+}
+
+void PrintSchedClasses()
+{
+	cout << output.size() << endl;
+	for(int i=0; i<(int)output.size(); i++)
+	{
+		cout << output[i];
+	}
+}
 int main()
 {
+	freopen("input.txt", "r", stdin);
+	freopen("timetable.txt", "w", stdout);
  	//taking input
     int n;
     // professors
@@ -120,7 +200,7 @@ int main()
     while(n--)
     {
         PROFESSOR p;
-        cin >> p.id >> p.name >> p.email;
+        cin >> p;
         p.id--;
         profs[p.id]=p;
     }
@@ -130,7 +210,7 @@ int main()
 	while(n--)
 	{
 		TA t;
-		cin >> t.id >> t.name >> t.email;
+		cin >> t;
 		t.id--;
 		TAs[t.id]=t;
 	}
@@ -141,33 +221,30 @@ int main()
     {
         COURSE c;
         int pid;
-        cin >> c.id >> c.code >> c.name >> pid;
+        cin >> c >> pid;
         pid--;
         c.id--;
         c.prof = &profs[pid];
         courses[c.id] = c;
     }
-
-    //classes
-    cin >> n;
-    classes.resize(n);
-    while(n--)
-    {
-        CLASS c;
-        int cid;
-        cin >> c.id >> c.type >> cid;
-        c.id--;
-        cid--;
-        c.course = &courses[cid];
-        classes[c.id] = c;
-    }
+	//rooms
+	cin >> n;
+	R.resize(n);
+	while(n--)
+	{
+		ROOM r;
+		cin >> r;
+		r.id--;
+		rooms[r.type-1].push_back(r);
+		R[r.id] = r;
+	}	
     //groups
     cin >> n;
     groups.resize(n);
     while(n--)
     {
         GROUP g;
-        cin >> g.id >> g.degree >> g.number >> g.year;
+        cin >> g;
         g.id--;
         groups[g.id] = g;
     }
@@ -178,21 +255,12 @@ int main()
     {
         PREFERENCES p;
         int pid;
-        cin >> p.id >> p.time >> pid;
+        cin >> p >> pid;
         pid--;
         p.id--;
         p.prof = &profs[pid];
     	prefs[p.id] = p;
 	}
-    //CLASS_GROUP
-    cin >> n;
-    while(n--)
-    {
-    	int cid, gid;
-    	cin >> cid >> gid;
-    	cid--, gid--;
-    	class_groups[cid].push_back(gid);
-    }
     //TA_COURSE
     cin >> n;
     while(n--)
@@ -200,8 +268,10 @@ int main()
     	int tid, cid;
     	cin >> tid >> cid;
 		tid--, cid--;
-		course_TAs[cid].push_back(tid);
-	} 
-    fill_lectures_and_tutorials();
-	fill_labs();
+		courses[cid].TAs.push_back(tid);
+	}
+    GenerateTimetable();
+	FillSchedClasses();
+//	PrintSchedClasses();
+	PrintTimetable();
 }
--- Algorithm/structures.h
+++ Algorithm/structures.h
@@ -7,37 +7,117 @@ struct PROFESSOR {
 	string name, email;
 };
 
+istream& operator >> (istream& is, PROFESSOR& p)
+{
+	is >> p.id >> p.name >> p.email;
+	return is;
+}
+
 struct COURSE {
-	int id;
-	string code, name;	
+	int id, year;
+	string code, name, degree;	
 	PROFESSOR* prof;
+	vector<int> TAs;
 };
+
+istream& operator >> (istream& is, COURSE& c)
+{
+	int id;
+	is >> c.id >> c.code >> c.name >> c.degree >> c.year;
+	return is;
+}
 
 struct TA {
 	int id;
 	string name, email;
 };
 
+istream& operator >> (istream& is, TA& t)
+{
+	is >> t.id >> t.name >> t.email;
+	return is;
+}
+
 struct CLASS {
-	int id, day, type, timeslot;
+	int id, type;
 	COURSE* course;
 };
+
+istream& operator >> (istream& is, CLASS& c)
+{
+	is >> c.id >> c.type;
+	return is;
+}
 
 struct ROOM {
 	int id, number, type;	
 };
+
+istream& operator >> (istream& is, ROOM& r)
+{
+	is >> r.id >> r.number >> r.type;
+	return is;
+}
 
 struct GROUP {
 	int id, number, year;	
 	string degree;
 };
 
+istream& operator >> (istream& is, GROUP& g)
+{
+	cin >> g.id >> g.degree >> g.number >> g.year;
+	return is;
+}
+
 struct PREFERENCES {
 	int id, time;
 	PROFESSOR* prof;
 };
 
+istream& operator >> (istream& is, PREFERENCES& p)
+{
+	is >> p.id >> p.time;
+	return is;
+}
+
 struct CELL {
-	int type, room;
+	int type=-1, room_id;
+	int instructor_id;
 	string instructor_name, course_name;
 };
+
+struct SCHED_CLASS {
+	int id;
+	int day, timeslot;
+	int type=-1;
+	int instructor_id, room_id, group_id;
+};
+
+ostream& operator << (ostream& os, SCHED_CLASS& c)
+{
+	os << c.id << " " << c.day << " " << c.timeslot << " " << c.type
+	<< " " << c.instructor_id << " "  << c.group_id << endl;
+	return os;
+}
+
+const int N = 505;
+const int G = 4, Y = 3, D = 5, T = 7;
+vector<PROFESSOR> profs;
+vector<COURSE> courses;
+vector<ROOM> rooms[3];
+vector<ROOM> R;
+vector<CLASS> classes;
+vector<GROUP> groups;
+vector<PREFERENCES> prefs;
+vector<SCHED_CLASS> output;
+vector<TA> TAs;
+string days[] = {"Mon", "Tue", "Wed", "Thu", "Fri"};
+string slot[] = {
+	"09:00-10:30", "10:40-12:10", "12:40-14:10", "14:20-15:50", "16:00-17:30", "17:40-19:10", "19:20-20:50"
+};
+string types[] = {"Lec  ", "Tut  ", "Lab  "};
+
+int cnt[D][T][3];
+CELL grid[D][T][Y][G]; //[days][timeslot][year][group];
+
